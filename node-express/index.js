e43e703ae9152a9f689323b49dc9c5821623b88e
klforthwind <kl.forthwind@gmail.com>
2019-05-31 00:23:58 +0000

    more examples

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
 
+// Get Request
 app.get("/", (request, response) => {
-    response.send("This is an Express Api!");
+    response.send(`This is an Express Api!`);
 });
 
-app.get("/dragon", (req, res) => {
-    response.send("You have reached the dragon");
+// Post Request
+app.post("/dragon", (req, res) => {
+    res.send(`You have reached the dragon`);
+});
+
+// Any Request
+app.post("/wolf", (req, res) => {
+    res.send(`You have reached the wolf`);
+});
+
+// Get Request With Query
+app.get("/wolf", (req, res) => {
+    res.send(`This is an ${req.query.name} Api!`);
+});
+
+// Get Request With Query
+app.get("/dragon:name", (req, res) => {
+    res.send(`This is an ${req.param.name} Api!`);
 });
 
 app.listen(4000);
